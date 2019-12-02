0f740580db031d16816273ea0857b2654a5bd0fb
AethanKarp <54898729+AethanKarp@users.noreply.github.com>
2019-12-02 09:38:42 -0500

    Update 01-function-header-practice.py

--- 05-functions/01-function-header-practice.py
+++ 05-functions/01-function-header-practice.py
@@ -1,18 +1,19 @@
 # Write function headers and docstring for the following:
 
 # sum
-
+def sum_of_ab(num1, num2):
 
 # math.ceil
-
+def calc_ceil(num: float) -> int:
 
 # math.floor
-
+def calc_floor(num: float) -> int:
 
 # Function that takes a name and returns a message
-
+def message(word):
 
 # Function that takes two floats and finds their average.
-
+def calc_average(num1, num2: float) -> float 
 
 # Function that takes an age and returns True if they are of voting age
+def confirm(age):
